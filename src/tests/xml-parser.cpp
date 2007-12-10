67b17750509f5753f3e3488c0af57b395679b4be
mina86 <mina86>
2007-12-10 12:35:07 +0000

    + added PPCP parser tester

--- src/tests/xml-parser.cpp
+++ src/tests/xml-parser.cpp
@@ -1,6 +1,6 @@
 /** \file
- * XML parser tester.
- * $Id: xml-parser.cpp,v 1.1 2007/12/08 18:02:11 mina86 Exp $
+ * XML & PPCP parser tester.
+ * $Id: xml-parser.cpp,v 1.2 2007/12/10 12:35:07 mina86 Exp $
  */
 
 #include <stdio.h>
@@ -9,25 +9,27 @@
 #include <map>
 
 #include "../xml-parser.hpp"
+#include "../ppcp-parser.hpp"
 
 namespace test {
 
 class XMLParser : public ppc::xml::TokenConsumer {
 protected:
 	virtual void open(const std::string &element) {
-		printf("open(%s)\n", element.c_str());
+		printf("EV %16s: %s\n", "open", element.c_str());
 	}
 
 	virtual void attribute(const std::string &name,const std::string &value) {
-		printf("attribute(%s = %s)\n", name.c_str(), value.c_str());
+		printf("EV %16s: %s = %s\n", "attribute", name.c_str(),
+		       value.c_str());
 	}
 
 	virtual void close(const std::string &name) {
-		printf("close(%s)\n", name.c_str());
+		printf("EV %16s: %s\n", "close", name.c_str());
 	}
 
 	virtual void cdata(const std::string &data) {
-		printf("cdata(%s)\n", data.c_str());
+		printf("EV %16s: '%s'\n", "text", data.c_str());
 	}
 };
 
@@ -35,36 +37,60 @@ protected:
 }
 
 /** An XML parser test. */
-int main() {
-	std::map<unsigned, const char *> tokenNames;
+int main(int argc, char **argv) {
+	std::map<unsigned, const char *> xTokenNames;
+	std::map<unsigned, const char *> pTokenNames;;
 	ppc::xml::Tokenizer tokenizer;
 	test::XMLParser parser;
+	ppc::ppcp::Tokenizer pTokenizer(argc < 2 ? "mina86" : argv[1], argc > 2);
 	char buffer[128];
 
-	tokenNames.insert(std::make_pair(ppc::xml::Tokenizer::END, "END"));
-	tokenNames.insert(std::make_pair(ppc::xml::Tokenizer::TAG_OPEN,
-	                                 "TAG_OPEN"));
-	tokenNames.insert(std::make_pair(ppc::xml::Tokenizer::ATTR_NAME,
-	                                 "ATTR_NAME"));
-	tokenNames.insert(std::make_pair(ppc::xml::Tokenizer::ATTR_VALUE,
-	                                 "ATTR_VALUE"));
-	tokenNames.insert(std::make_pair(ppc::xml::Tokenizer::TAG_CLOSE,
-	                                 "TAG_CLOSE"));
-	tokenNames.insert(std::make_pair(ppc::xml::Tokenizer::TEXT,
-	                                 "TEXT"));
-	tokenNames.insert(std::make_pair(ppc::xml::Tokenizer::ELEMENT_CLOSE,
-	                                 "ELEMENT_CLOSE"));
+	xTokenNames.insert(std::make_pair(ppc::xml::Tokenizer::END, "END"));
+	xTokenNames.insert(std::make_pair(ppc::xml::Tokenizer::TAG_OPEN,
+	                                  "TAG_OPEN"));
+	xTokenNames.insert(std::make_pair(ppc::xml::Tokenizer::ATTR_NAME,
+	                                  "ATTR_NAME"));
+	xTokenNames.insert(std::make_pair(ppc::xml::Tokenizer::ATTR_VALUE,
+	                                  "ATTR_VALUE"));
+	xTokenNames.insert(std::make_pair(ppc::xml::Tokenizer::TAG_CLOSE,
+	                                  "TAG_CLOSE"));
+	xTokenNames.insert(std::make_pair(ppc::xml::Tokenizer::TEXT, "TEXT"));
+	xTokenNames.insert(std::make_pair(ppc::xml::Tokenizer::ELEMENT_CLOSE,
+	                                  "ELEMENT_CLOSE"));
+
+	pTokenNames.insert(std::make_pair(ppc::ppcp::Tokenizer::END, "END"));
+	pTokenNames.insert(std::make_pair(ppc::ppcp::Tokenizer::IGNORE,
+	                                  "IGNORE"));
+	pTokenNames.insert(std::make_pair(ppc::ppcp::Tokenizer::PPCP_OPEN,
+	                                  "PPCP_OPEN"));
+	pTokenNames.insert(std::make_pair(ppc::ppcp::Tokenizer::ST, "ST"));
+	pTokenNames.insert(std::make_pair(ppc::ppcp::Tokenizer::RQ, "RQ"));
+	pTokenNames.insert(std::make_pair(ppc::ppcp::Tokenizer::M, "M"));
+	pTokenNames.insert(std::make_pair(ppc::ppcp::Tokenizer::PPCP_CLOSE,
+	                                  "PPCP_CLOSE"));
 
 	try {
 		tokenizer.init();
+		pTokenizer.init();
 		while (fgets(buffer, sizeof buffer, stdin)) {
-			ppc::xml::Tokenizer::Token token;
+			ppc::xml::Tokenizer::Token xToken;
+			ppc::ppcp::Tokenizer::Token pToken;
+
 			tokenizer.feed(buffer, strlen(buffer));
 			do {
-				token = tokenizer.nextToken();
-				printf("token %15s: '%s'\n", tokenNames[token.type],
-				       token.data.c_str());
-			} while (parser.consumeToken(token));
+				xToken = tokenizer.nextToken();
+				if (xToken) {
+					printf("XML %15s: '%s'\n", xTokenNames[xToken.type],
+					       xToken.data.c_str());
+				}
+				parser.consumeToken(xToken);
+				pToken = pTokenizer.nextToken(xToken);
+				if (pToken) {
+					printf("PPCP %14s: '%s' (%u)\n",
+					       pTokenNames[pToken.type], pToken.data.c_str(),
+					       pToken.flags);
+				}
+			} while (xToken);
 		}
 		tokenizer.done();
 	}
