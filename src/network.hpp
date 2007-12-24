b27d44faeef0edf299c2bb3e12204597fc056c5f
mina86 <mina86>
2007-12-24 12:31:51 +0000

    + Configuration enums added + Network::recieveUDPDatagrams() renamed to Network::readFromUDPSocket() + Various methods added

--- src/network.hpp
+++ src/network.hpp
@@ -1,6 +1,6 @@
 /** \file
  * Network module definition.
- * $Id: network.hpp,v 1.2 2007/12/23 01:13:22 mina86 Exp $
+ * $Id: network.hpp,v 1.3 2007/12/24 12:31:51 mina86 Exp $
  */
 
 #ifndef H_NETWORK_HPP
@@ -16,6 +16,7 @@ namespace ppc {
 
 
 struct NetworkConnection;
+struct NetworkUser;
 
 
 /**
@@ -45,26 +46,106 @@ struct Network : public Module {
 
 
 private:
+	/** Certain constants. */
+	enum {
+		/**
+		 * Time after which user (with state different then offline)
+		 * is considered to disconnected.
+		 */
+		ONLINE_USER_MAX_AGE  = 750,
+
+		/**
+		 * Time after which user (with offline state) is considered to
+		 * disconnect.  Normally, if user sends a packet with offline
+		 * status (s)he is treated as if (s)he has disconnected, yet
+		 * if user sends us a message (s)he apperas with offline
+		 * status and this time specifis how long shall it be kept
+		 * without any activity.
+		 */
+		OFFLINE_USER_MAX_AGE = 400,
+
+		/**
+		 * Status sending interval.
+		 */
+		STATUS_RESEND        = 300
+	};
+
+
 	/** Vector of all opened TCP connections. */
-	typedef unordered_vector<NetworkConnection> TCPSockets;
+	typedef unordered_vector<NetworkConnection*> TCPSockets;
 
 	/** A sequence used to give each module unique name. */
 	static unsigned network_id;
 
+
 	/**
 	 * accept()s connections from listening socket and adds them to
 	 * tcpSockets vector.
+	 * \throw NetException if error while accepting connection occured.
 	 */
 	void acceptConnections();
 
-	/** Reads data from udpSocket and parses it. */
-	void recieveUDPDatagrams();
+
+	/**
+	 * Reads data from udpSocket and parses it.
+	 * \throw NetException if error while recieving data from UDP
+	 *                     socket occured.
+	 * \throw xml::Error when data feed to tokenizer was not valid XML.
+	 */
+	void readFromUDPSocket();
+
+
 
 	/**
 	 * Reads data from given TCP connection and parses it.
 	 * \param conn connection to handle.
+	 * \throw NetException if error while recieving data from TCP
+	 *                     socket occured.
+	 * \throw xml::Error   if data was misformatted XML stream.
 	 */
 	void readFromTCPConnection(NetworkConnection &conn);
+
+	/**
+	 * Writes pending data to given TCP connection and parses it.
+	 * \param conn connection to handle.
+	 * \throw NetException if error while sending data from TCP
+	 *                     socket occured.
+	 */
+	void writeToTCPConnection(NetworkConnection &conn);
+
+	/**
+	 * Closes TCP connection and removes all references to users.
+	 * \param conn connection to close.
+	 */
+	void closeConnection(NetworkConnection &conn);
+
+
+
+	/**
+	 * User changed status.
+	 * \param user   user that did this.
+	 * \param status new state.
+	 */
+	void gotStatus(NetworkUser &user, User::Status status);
+
+	/**
+	 * Sends status to given user or whole network.
+	 * \param user user to send status to.
+	 */
+	void sendStatus(NetworkUser *user);
+
+	/**
+	 * User sent a message.
+	 * \param user  user who sent message.
+	 * \param data  message's text.
+	 * \param flags combination of sig::MessageData::ACTION and
+	 *              sig::MessageData::MESSAGE flags.
+	 */
+	void gotMessage(NetworkUser &user, const std::string &data,
+	                unsigned flags);
+
+
+	NetworkUser *getUser(const User::ID &id);
 
 
 	/** Network's address. */
