a5ee1ed1d58bb4c71a886697a1138562e118ac6c
mina86 <mina86>
2007-12-27 17:41:39 +0000

    + Initial revision

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/** \file
+ * Main file.
+ * $Id: main.cpp,v 1.1 2007/12/27 17:41:39 mina86 Exp $
+ */
+
+#include <stdio.h>
+
+/** Main function. */
+int main(void) {
+	puts("hello, world");
+	return 0;
+}
