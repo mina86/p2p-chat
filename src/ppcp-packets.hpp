26050e2b3035c71af2cad06e7978b5be5b39ee3e
mina86 <mina86>
2007-12-27 00:34:47 +0000

    + two new ppcpOpen() functions taking User object as argument

--- src/ppcp-packets.hpp
+++ src/ppcp-packets.hpp
@@ -1,6 +1,6 @@
 /** \file
  * Methods generating ppcp packets.
- * $Id: ppcp-packets.hpp,v 1.3 2007/12/25 15:35:47 mina86 Exp $
+ * $Id: ppcp-packets.hpp,v 1.4 2007/12/27 00:34:47 mina86 Exp $
  */
 
 #ifndef H_PPCP_PACKETS_HPP
@@ -28,6 +28,16 @@ inline std::string ppcpOpen(const std::string &nick) {
 
 /**
  * Returns a \c ppcp open tag.
+ * \param user user object.
+ */
+inline std::string ppcpOpen(const User &user) {
+	return ppcpOpen(User::nickFromName(user.name) == user.id.nick
+	                ? user.name : user.id.nick);
+}
+
+
+/**
+ * Returns a \c ppcp open tag.
  * \param nick value of \c n attribute.
  * \param to   value of \c to:n attribute.
  * \param neg  if \c true \c to:neg will be set to \c "neg".
@@ -36,6 +46,19 @@ inline std::string ppcpOpen(const std::string &nick, const std::string &to,
                             bool neg = false) {
 	return "<ppcp n=\"" + xml::escape(nick) + "\" to:n=\"" +
 		xml::escape(to) + (neg ? "\" to:neg=\"neg\">" : "\">");
+}
+
+
+/**
+ * Returns a \c ppcp open tag.
+ * \param user user object.
+ * \param to   value of \c to:n attribute.
+ * \param neg  if \c true \c to:neg will be set to \c "neg".
+ */
+inline std::string ppcpOpen(const User &user, const std::string &to,
+                            bool neg = false ) {
+	return ppcpOpen(User::nickFromName(user.name) == user.id.nick
+	                ? user.name : user.id.nick, to, neg);
 }
 
 
