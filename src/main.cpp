d5a683250a8218378ca88317e808a8444e2f24e9
mina86 <mina86>
2008-01-06 15:24:09 +0000

    + dummy entry replaced with a bit less dummy entry

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 /** \file
  * Main file.
- * $Id: main.cpp,v 1.1 2007/12/27 17:41:39 mina86 Exp $
+ * $Id: main.cpp,v 1.2 2008/01/06 15:24:09 mina86 Exp $
  */
 
 #include <stdio.h>
+#include "application.hpp"
+#include "ui.hpp"
 
 /** Main function. */
 int main(void) {
-	puts("hello, world");
+
+	struct ppc::Config c;
+	struct ppc::Core core(c);
+	struct ppc::UI *ui = new ppc::UI(core);
+	core.addModule(*ui);
+	core.run();
 	return 0;
 }
