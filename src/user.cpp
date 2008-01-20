73f9c8e5813f2c8bb39705ee05ff00eb6742ec05
mina86 <mina86>
2008-01-20 16:54:58 +0000

    + characters tested without cast (gives warning about comparison   always true/false because of limited range of operands but well)

--- src/user.cpp
+++ src/user.cpp
@@ -1,6 +1,6 @@
 /** \file
  * User structures definitions.
- * $Id: user.cpp,v 1.3 2008/01/13 21:55:54 mina86 Exp $
+ * $Id: user.cpp,v 1.4 2008/01/20 16:54:58 mina86 Exp $
  */
 
 #include "user.hpp"
@@ -13,7 +13,7 @@ bool User::isValidNick(const std::string &nick) {
 	bool ok = it != end;
 	for (; ok && it!=end; ++it) {
 		const char ch = *it;
-		ok = ok && static_cast<signed char>(ch)>=48 && (ch<'A' || ch>'Z');
+		ok = ok && ch>=48 && ch<128 && (ch<'A' || ch>'Z');
 	}
 	return ok;
 }
@@ -23,7 +23,7 @@ bool User::isValidName(const std::string &name) {
 	const char *it = name.data(), *const end = it + name.length();
 	bool ok = it != end;
 	for (; ok && it!=end; ++it) {
-		ok = ok && static_cast<signed char>(*it)>=48;
+		ok = ok && *it >= 48 && *it < 128;
 	}
 	return ok;
 }
@@ -32,7 +32,7 @@ bool User::isValidName(const std::string &name) {
 std::string &User::nickFromNameInPlace(std::string &name) {
 	char *it = &name[0], *const end = it + name.length();
 	for (; it != end; ++it) {
-		if (static_cast<signed char>(*it) < 48) {
+		if (*it < 48 || *it >= 128) {
 			*it = '_';
 		} else if (*it >= 'A' && *it <= 'Z') {
 			*it |= 32;
